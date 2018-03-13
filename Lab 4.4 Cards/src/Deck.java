875b916afd39ac0ffd29dd782ab152afb8b61c89
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-03-13 09:22:12 -0400

    Deck start

--- Lab 4.4 Cards/src/Deck.java
+++ Lab 4.4 Cards/src/Deck.java
@@ -1,4 +1,13 @@
-
-public class Deck {
-
+/**
+ * 
+ * @author Alyssa Ma
+ * 3/13/18
+ * Period 2
+ */
+public class Deck (String [] rank, String [] suit, int [] value)
+{
+	for(int i = 0; i < ranks.length; i++)
+	{
+		for(int j = 0; j < suits.length ; j++)
+	}
 }
