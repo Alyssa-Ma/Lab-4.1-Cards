d6e7ca78025aef96bb074c5276d51825f02a7c69
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-03-14 09:22:21 -0400

    working on deck

--- Lab 4.4 Cards/src/Deck.java
+++ Lab 4.4 Cards/src/Deck.java
@@ -6,8 +6,14 @@
  */
 public class Deck (String [] rank, String [] suit, int [] value)
 {
+	ArrayList<Card> unDealt = new ArrayList<Card>();
+	ArrayList<Card> Dealt = new ArrayList<Card>();
+	
 	for(int i = 0; i < ranks.length; i++)
 	{
 		for(int j = 0; j < suits.length ; j++)
+		{
+			unDealt.add(newCard(ranks[i], suits[j], value[i]));
+		}
 	}
 }
---
+++ Lab 4.4 Cards/src/Running_Runner.java
@@ -0,0 +1,8 @@
+
+public class Running_Runner 
+{
+	public static void main (String [] args)
+	{
+		
+	}
+}
